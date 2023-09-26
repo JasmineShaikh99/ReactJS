109887d79a02f49580d8d2bdf439be441abe7d45
jasmineshaikh20 <jasmine_shaikh@colpal.com>
2023-09-26 11:16:25 +0530

    React Multiple Elements

--- app.js
+++ app.js
@@ -11,11 +11,34 @@
 
 // React Code to print Hello World
 
+// const heading = React.createElement('h1', {}, 'Hello World')
 
-const heading = React.createElement('h1', {}, 'Hello World')
-
-const parent = ReactDOM.createRoot(document.getElementById('root'))
+// const parent = ReactDOM.createRoot(document.getElementById('root'))
 
 // HEading gets replaced by root's inner HTML
-parent.render(heading)
+// parent.render(heading)
 
+
+// Write React Code for 
+/* <div id="parent">
+        <div id="Child1">
+            <h1>I m h1 tag in Child1</h1>
+        </div>
+        <div id="Child2">
+            <h1>I m h1 tag in Child2</h1>
+        </div>
+</div> */
+
+
+const Child1 = React.createElement('div', {id: 'Child1'}, 
+                                    React.createElement('h1', {}, 'I m h1 tag in Child1'))
+
+
+const Child2 = React.createElement('div', {id: 'Child2'}, 
+                                    React.createElement('h1', {}, 'I m h1 tag in Child2'))
+
+const parent = React.createElement('div', {id:'parent'}, [Child1, Child2])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent)
+                                    
